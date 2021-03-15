afc6984715dbc29aaf57853e7709fdd87e797cd2
rachel <rachelcope.m@gmail.com>
2021-03-15 15:48:54 -0600

    Adding three files from lecture 1 eclipse workspace into local repo.

---
+++ Author.java
@@ -0,0 +1,15 @@
+package lecture1;
+
+
+public class Author {
+  String name;
+  int yob;
+  
+  Author(String name, int yob) {
+    this.name = name;
+    this.yob = yob;
+        
+  }
+
+
+}
---
+++ Publisher.java
@@ -0,0 +1,16 @@
+package lecture1;
+
+public class Publisher {
+  String name;
+  String country;
+  int yearOpened;
+  
+  Publisher(String name, String country, int yearOpened) {
+    this.name = name;
+    this.country = country;
+    this.yearOpened = yearOpened;
+    
+    
+  }
+
+}
