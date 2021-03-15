6dffaee6803e9a627cbc514bd188a7deeefa5eca
rachel <rachelcope.m@gmail.com>
2021-03-15 16:05:49 -0600

    Added all directories from eclipse-workspace fundies2 into Github

--- Practice.java
+++
@@ -1,9 +0,0 @@
-package lecture1;
-
-public class Practice {
-
-  Author rachel = new Author("Rachel", 1994);
-  Publisher penguinBooks = new Publisher("penguin book", "USA", 1994);
-  Book twilight = new Book("Twilight", 2006);
-  
-}
